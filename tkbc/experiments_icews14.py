c0998f8217704a01b6d20ab1812f1ec7d5158537
Manuel Dileo <dileomanuel0@gmail.com>
2023-06-14 17:29:14 +0100

    uncomment grid search

--- tkbc/experiments_icews14.py
+++ tkbc/experiments_icews14.py
@@ -32,20 +32,13 @@ def to_logfile(c, path):
 
 
 def main(argv):
-    """"
     hyp_space_1 = dict(
         rank=[128, 256],
         emb_reg=[1, 1e-1, 1e-2, 1e-3],
         time_reg=[1, 1e-1, 1e-2, 1e-3],
         time_norm=['Lambda3','N3','L1','L2','F2']
     )
-    """
-    hyp_space_1 = dict(
-           rank=[256],
-           emb_reg=[1e-2],
-           time_reg=[1, 1e-1, 1e-2, 1e-3],
-           time_norm=['L2']
-    )
+
     configurations = list(cartesian_product(hyp_space_1))
 
     path = 'logs/icews14'
