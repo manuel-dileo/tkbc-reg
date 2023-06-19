4f9241e787e700ecbdeb36b84384d8edd81f23bb
Manuel Dileo <dileomanuel0@gmail.com>
2023-06-19 15:14:37 +0100

    fix redundance

--- tkbc/experiments_icews14.py
+++ tkbc/experiments_icews14.py
@@ -19,7 +19,7 @@ def summary(configuration):
 
 
 def to_cmd(c, _path=None):
-    command = f'PYTHONPATH=. python3 ../learner.py --rank 156 --emb_reg 1e-2 --time_reg 1e-2 '\
+    command = f'PYTHONPATH=. python3 ../learner.py'\
         f'--dataset ICEWS14 '\
         f'--model TNTComplEx '\
         f'--rank {c["rank"]} --emb_reg {c["emb_reg"]} --time_reg {c["time_reg"]} --time_norm {c["time_norm"]}'
--- tkbc/experiments_yago15k.py
+++ tkbc/experiments_yago15k.py
@@ -19,7 +19,7 @@ def summary(configuration):
 
 
 def to_cmd(c, _path=None):
-    command = f'PYTHONPATH=. python3 ../learner.py --rank 156 --emb_reg 1e-2 --time_reg 1e-2 '\
+    command = f'PYTHONPATH=. python3 ../learner.py'\
         f'--dataset yago15k '\
         f'--model TNTComplEx '\
         f'--rank {c["rank"]} --emb_reg {c["emb_reg"]} --no_time_emb --time_reg {c["time_reg"]} --time_norm {c["time_norm"]}'
