d58f404f54b46fd423792830707d88b68791655a
Manuel Dileo <dileomanuel0@gmail.com>
2023-07-28 15:27:29 +0100

    linear3 icews15

--- tkbc/experiments_icews15.py
+++ tkbc/experiments_icews15.py
@@ -26,7 +26,7 @@ def to_cmd(c, _path=None):
         f'--rank {c["rank"]} --emb_reg {c["emb_reg"]} --time_reg {c["time_reg"]} --time_norm {c["time_norm"]} --time_reg_w {c["time_reg_w"]} --p_norm {c["p_norm"]}'
     """
     command = f'PYTHONPATH=. python3 ../learner.py ' \
-              f'--dataset yago15k ' \
+              f'--dataset ICEWS05-15 ' \
               f'--model {c["model"]} ' \
               f'--rank {c["rank"]} --emb_reg {c["emb_reg"]} --time_reg {c["time_reg"]} --time_reg_w {c["time_reg_w"]}'
     return command
