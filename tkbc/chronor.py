afa0a6899b00f27c4ea19114ac7c6db4526116de
Manuel Dileo <dileomanuel0@gmail.com>
2023-08-07 00:55:27 +0100

    test fix bug

--- tkbc/chronor.py
+++ tkbc/chronor.py
@@ -80,7 +80,7 @@ class ChronoR(TKBCModel):
         return torch.sum(hrt[0]*rhs[0]+hrt[1]*rhs[1],1,keepdim=True)
     
     def forward(self, x):
-        if rank % 2 != 0:
+        if self.rank % 2 != 0:
             raise Exception('Rank must be even')
         lhs = self.embeddings[0](x[:, 0])
         rel = self.embeddings[1](x[:, 1])
